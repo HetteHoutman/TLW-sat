21b7245aa56983bedf240ec7fe2d3b9405b0cc97
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-06-02 16:05:45 +0100

    load from settings.json

--- file_to_image.py
+++ file_to_image.py
@@ -1,14 +1,12 @@
-import json
 import sys
-from types import SimpleNamespace
 
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+from miscellaneous import make_great_circle_points, check_argv_num, load_settings
 from pyresample import get_area_def
 from satpy import Scene
 from satpy.writers import get_enhanced_image
 
-from miscellaneous import make_great_circle_points
 
 def produce_scene(filename, area_extent=None):
     # load file
@@ -39,10 +37,10 @@ def produce_image(scene2, crs, filename, coastlines=False, save_name=None, save=
         ax.coastlines()
     # ax.plot([-9.19, -8.82], [51.74, 51.99], marker='x', transform=crs, color='red')
     # ax.plot([-9.66, -9.37], [51.73, 51.99], marker='x', transform=crs, color='blue')
-    
+
     if great_circle is not None:
         ax.plot(great_circle[0], great_circle[1], color='r', zorder=50)
-    
+
     if save:
         if save_name is None:
             save_name = f'{filename[-32:-28]}-{filename[-28:-26]}-{filename[-26:-24]}_{filename[-24:-22]}_{filename[-22:-20]}'
@@ -51,24 +49,16 @@ def produce_image(scene2, crs, filename, coastlines=False, save_name=None, save=
         plt.savefig('images/' + save_name + '.png', dpi=300)
     return fig, ax
 
-def load_settings():
-    """borrowed from tephi_plot"""
-    if len(sys.argv) != 2:
-        raise Exception(f'Gave {len(sys.argv) - 1} arguments but this file takes exactly 1 (settings.json)')
-    file = sys.argv[1]
-    with open(file) as f:
-        settings = json.load(f, object_hook=lambda d: SimpleNamespace(**d))
-
-    return settings
-
 
 if __name__ == '__main__':
-    s = load_settings()
-    # TODO include satellite file in settings json
-    file = 'data/MSG3-SEVI-MSG15-0100-NA-20150414122741.433000000Z-NA/MSG3-SEVI-MSG15-0100-NA-20150414122741.433000000Z-NA.nat'
+    # check argument number and load settings
+    check_argv_num(sys.argv, 1, "(settings json file)")
+    s = load_settings(sys.argv[1])
+
     gc, dists = make_great_circle_points(s.gc_start, s.gc_end, n=s.n)
-    scene, crs = produce_scene(file,
+    scene, crs = produce_scene(s.sat_file,
                                area_extent=[*s.map_bottomleft, *s.map_topright]
                                )
-    fig, ax = produce_image(scene, crs, file, coastlines=True, save=True, save_name='test', great_circle=gc)
+
+    fig, ax = produce_image(scene, crs, s.sat_file, coastlines=True, save=True, save_name='test', great_circle=gc)
     plt.show()
