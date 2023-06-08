da546620e5024f88b408f0c154ae5d49d19f13f9
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-06-08 12:42:39 +0100

    now plots correct axes

--- fourier_analysis.py
+++ fourier_analysis.py
@@ -11,28 +11,32 @@ from miscellaneous import load_settings
 #            '.311000000Z-NA.nat'
 
 
-def basic_plot(x_dist, y_dist):
-    fig = plt.figure()
-    ax1 = plt.subplot(121, projection=crs)
+def basic_plot(img, shifted_ft, Lx, Ly):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
 
-    ax1.imshow(img, transform=crs, origin='upper', cmap='gray')
-    # ax1.imshow(img, transform=crs, origin='upper', extent=[-180, 180, -250, 250], cmap='gray')
-    xticks = np.linspace(0, 549, 5)
-    xticklabels = [f'{-180 + 360 / 550 * tick:.0f}' for tick in xticks]
-    ax1.set_xticks(xticks)
-    ax1.set_xticklabels(xticklabels)
-    yticks = np.linspace(0, 449, 5)
-    yticklabels = [f'{-250 + 500 / 450 * tick:.0f}' for tick in yticks]
-    ax1.set_yticks(yticks)
-    ax1.set_yticklabels(yticklabels)
+    xlen = img.shape[1]
+    ylen = img.shape[0]
 
-    ax2 = plt.subplot(122)
-    # hide ticks and labels
-    # ax2.xaxis.set_tick_params(labelbottom=False)
-    # ax2.yaxis.set_tick_params(labelleft=False)
-    # ax2.set_xticks([])
-    # ax2.set_yticks([])
-    ax2.imshow(abs(shifted_ft), norm='log')
+    pixel_x = Lx / xlen
+    pixel_y = Ly / ylen
+
+    ax1.imshow(img,
+               extent=[-Lx/2 - pixel_x/2, Lx/2 + pixel_x/2, -Ly/2 - pixel_y/2, Ly/2 + pixel_y/2],
+               cmap='gray')
+    ax1.set_xlabel('x distance / km')
+    ax1.set_ylabel('y distance / km')
+
+    max_k = xlen // 2 * 2 * np.pi / Lx
+    max_l = ylen // 2 * 2 * np.pi / Ly
+    pixel_k = 2 * max_k / xlen
+    pixel_l = 2 * max_l / ylen
+
+    ax2.imshow(abs(shifted_ft),
+               extent=[-max_k - pixel_k / 2, max_k + pixel_k / 2, -max_l - pixel_l / 2, max_l + pixel_l / 2],
+               norm='log')
+    ax2.set_xlabel('k / km^-1')
+    ax2.set_ylabel('l / km^-1')
+
     plt.tight_layout()
     plt.show()
 
@@ -57,7 +61,12 @@ def middle_line_masked_plot():
     axes[1].imshow(abs(np.fft.ifft2(temp)), cmap='gray')
     plt.show()
 
-def ideal_bandpass(ft, dist_array, low, high):
+
+def ideal_bandpass(ft, Lx, Ly, low, high):
+
+    xlen = ft.shape[1]
+    ylen = ft.shape[0]
+
     low_mask = (dist_array < low)
     high_mask = (dist_array > high)
 
@@ -79,16 +88,8 @@ if __name__ == '__main__':
 
     # low_values_masked_plot()
     # middle_line_masked_plot()
-    x_dist = np.linspace(-180, 180, 551, endpoint=True)
-    y_dist = np.linspace(-250, 250, 451, endpoint=True)
-    x, y = np.meshgrid(x_dist, y_dist)
 
-    dist_array = np.sqrt(x**2 + y**2)
-    basic_plot(x_dist, y_dist)
-
-
-
-
+    basic_plot(img, shifted_ft, 360, 500)
 
     # plt.hist(img.to_numpy().flatten(), histtype='step', bins=100)
     # plt.show()
