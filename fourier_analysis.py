e60b09cbafa172e22ab7767de76a122d0b944c2f
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-06-08 16:17:52 +0100

    improved

--- fourier_analysis.py
+++ fourier_analysis.py
@@ -1,6 +1,8 @@
 import numpy as np
 from file_to_image import produce_scene
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import pyproj
 
 
 # s = load_settings('../tephi_plot/settings/20150414_12_ireland.json')
@@ -50,53 +52,70 @@ def ideal_bandpass(ft, Lx, Ly, low, high):
 
     return temp
 
-def filtered_inv_plot(img, filtered_ft, Lx, Ly, inverse=True):
+
+def filtered_inv_plot(img, filtered_ft, Lx, Ly, latlon=None, inverse_fft=True):
     # TODO want to be able to plot lambda instead of k/l
-    if inverse:
-        fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+
+    if inverse_fft:
+        fig, (ax1, ax3) = plt.subplots(1, 2, sharey=True)
     else:
-        fig, (ax1, ax2) = plt.subplots(1, 2)
+        fig, ax1 = plt.subplots(1, 1)
+
     xlen = img.shape[1]
     ylen = img.shape[0]
 
-    pixel_x = Lx / xlen
-    pixel_y = Ly / ylen
+    if latlon:
+        physical_extent = [latlon[0], latlon[2], latlon[1], latlon[3]]
+        xlabel = 'Longitude'
+        ylabel = 'Latitude'
+    else:
+        pixel_x = Lx / xlen
+        pixel_y = Ly / ylen
+        physical_extent = [-Lx / 2 - pixel_x / 2, Lx / 2 + pixel_x / 2, -Ly / 2 - pixel_y / 2, Ly / 2 + pixel_y / 2]
+        xlabel = 'x distance / km'
+        ylabel = 'y distance / km'
 
     ax1.imshow(img,
-               extent=[-Lx / 2 - pixel_x / 2, Lx / 2 + pixel_x / 2, -Ly / 2 - pixel_y / 2, Ly / 2 + pixel_y / 2],
+               extent=physical_extent,
                cmap='gray')
-    ax1.set_xlabel('x distance / km')
-    ax1.set_ylabel('y distance / km')
+    ax1.set_ylabel(ylabel)
+    ax1.set_xlabel(xlabel)
+
+    if inverse_fft:
+        inv = np.fft.ifft2(filtered_ft)
+        ax3.imshow(abs(inv),
+                   extent=physical_extent,
+                   cmap='gray')
+    # save?
+    plt.tight_layout()
+    plt.show()
+
+    fig2, ax2 = plt.subplots(1,1)
 
     max_k = xlen // 2 * 2 * np.pi / Lx
     max_l = ylen // 2 * 2 * np.pi / Ly
     pixel_k = 2 * max_k / xlen
     pixel_l = 2 * max_l / ylen
-
+    recip_extent = [-max_k - pixel_k / 2, max_k + pixel_k / 2, -max_l - pixel_l / 2, max_l + pixel_l / 2]
     ax2.imshow(abs(filtered_ft),
-               extent=[-max_k - pixel_k / 2, max_k + pixel_k / 2, -max_l - pixel_l / 2, max_l + pixel_l / 2],
+               extent=recip_extent,
                norm='linear')
     ax2.set_xlabel('k / km^-1')
     ax2.set_ylabel('l / km^-1')
 
-    if inverse:
-        inv = np.fft.ifft2(filtered_ft)
-        ax3.imshow(abs(inv),
-                   extent=[-Lx / 2 - pixel_x / 2, Lx / 2 + pixel_x / 2, -Ly / 2 - pixel_y / 2, Ly / 2 + pixel_y / 2],
-                   cmap='gray')
-
-        ax3.set_xlabel('x distance / km')
-        ax3.set_ylabel('y distance / km')
-
-    plt.tight_layout()
     plt.show()
 
 
 if __name__ == '__main__':
     filename = 'data/MSG3-SEVI-MSG15-0100-NA-20230419115741.383000000Z-NA/MSG3-SEVI-MSG15-0100-NA-20230419115741.383000000Z-NA.nat'
-    area_extent = [-10.5, 51, -5, 55.5]
-    Lx = 360
-    Ly = 500
+    area_extent = [-9, 54, -8, 55]
+    g = pyproj.Geod(ellps='WGS84')
+    _, _, Lx = g.inv(area_extent[0], (area_extent[3] + area_extent[1]) / 2,
+                     area_extent[2], (area_extent[3] + area_extent[1]) / 2)
+    _, _, Ly = g.inv((area_extent[0] + area_extent[2]) / 2, area_extent[1],
+                     (area_extent[0] + area_extent[2]) / 2, area_extent[3])
+    Lx /= 1000
+    Ly /= 1000
     # area_extent = [-11.5, 49.5, 2, 60]
     scene, crs = produce_scene(filename, area_extent=area_extent)
     # img = get_enhanced_image(scene['HRV']).data.transpose('y', 'x', 'bands')
@@ -106,8 +125,8 @@ if __name__ == '__main__':
     shifted_ft = np.fft.fftshift(ft)
 
     # basic_plot(orig, shifted_ft, Lx, Ly)
-    min_lambda = 0.1
-    max_lambda = 15
+    min_lambda = 2
+    max_lambda = 20
     filtered = ideal_bandpass(shifted_ft, Lx, Ly, 2 * np.pi / max_lambda, 2 * np.pi / min_lambda)
     inv = np.fft.ifft2(shifted_ft)
-    filtered_inv_plot(orig, filtered, Lx, Ly, inverse=True)
+    filtered_inv_plot(orig, filtered, Lx, Ly, latlon=area_extent, inverse_fft=True)
