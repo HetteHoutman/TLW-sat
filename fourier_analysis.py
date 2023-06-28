7a5620b75fd4b2ce453f9ce2674f0a93c81072c1
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-06-28 15:03:21 +0100

    can use pixels of 1kmx1km

--- fourier_analysis.py
+++ fourier_analysis.py
@@ -9,54 +9,13 @@ from miscellaneous import check_argv_num, load_settings
 from scipy import stats
 
 from file_to_image import produce_scene
+from functions import create_bins, recip_space, extract_distances, ideal_bandpass
 
 
 # s = load_settings('../tephi_plot/settings/20150414_12_ireland.json')
 
 # filename = 'data/MSG3-SEVI-MSG15-0100-NA-20150414124241.311000000Z-NA/MSG3-SEVI-MSG15-0100-NA-20150414124241' \
 #            '.311000000Z-NA.nat'
-
-def ideal_bandpass(ft, Lx, Ly, low, high):
-    _, _, dist_array, thetas = recip_space(Lx, Ly, ft.shape)
-
-    low_mask = (dist_array < low)
-    high_mask = (dist_array > high)
-    masked = np.ma.masked_where(low_mask | high_mask, ft)
-
-    return masked
-
-
-def recip_space(Lx, Ly, shape):
-    xlen = shape[1]
-    ylen = shape[0]
-
-    # np uses linear frequency f instead of angular frequency omega=2pi*f, so multiply by 2pi to get angular wavenum k
-    k = 2 * np.pi * np.fft.fftfreq(xlen, d=Lx / xlen)
-    l = 2 * np.pi * np.fft.fftfreq(ylen, d=Ly / ylen)
-
-    # do fft shift
-    K, L = np.meshgrid(np.roll(k, k.shape[0] // 2), np.roll(l, l.shape[0] // 2))
-
-    dist_array = np.sqrt(K ** 2 + L ** 2)
-    thetas = -np.rad2deg(np.arctan2(K, L)) + 180
-    thetas %= 180
-    return K, L, dist_array, thetas
-
-
-def extract_distances(lats, lons):
-    g = pyproj.Geod(ellps='WGS84')
-    _, _, Lx = g.inv(lons[0], lats[lats.shape[0] // 2],
-                     lons[-1], lats[lats.shape[0] // 2])
-    _, _, Ly = g.inv(lons[lons.shape[0] // 2], lats[0],
-                     lons[lons.shape[0] // 2], lats[-1])
-
-    return Lx / 1000, Ly / 1000
-
-
-def create_bins(range, bin_width):
-    bins = np.linspace(range[0], range[1], int(np.ceil(range[1] / bin_width) + 1))
-    vals = 0.5 * (bins[1:] + bins[:-1])
-    return bins, vals
 
 
 def make_radial_pspec(pspec_2d: np.ma.masked_array, wavenumbers, wavenumber_bin_width, thetas, theta_bin_width):
@@ -234,7 +193,8 @@ if __name__ == '__main__':
     # ireland
     area_extent = [-11, 51, -5, 55.5]
     # area_extent = [*s.map_bottomleft, *s.map_topright]
-    scene, crs = produce_scene(filename, area_extent=area_extent)
+    scene, crs = produce_scene(filename, bottomleft=area_extent[:2], topright=area_extent[2:],
+                               grid='km')
     Lx, Ly = extract_distances(scene['HRV'].y[::-1], scene['HRV'].x)
     orig = np.array(scene['HRV'].data)
 
