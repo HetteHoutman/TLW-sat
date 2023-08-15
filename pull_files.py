bf3becfea54072d6542dcb05f15ca126d3fb90d0
hhoutman <h.g.houtman@pgr.reading.ac.uk>
2023-08-15 11:33:41 +0200

    now extracts from excel file

--- pull_files.py
+++ pull_files.py
@@ -1,4 +1,6 @@
 import eumdac
+import numpy as np
+
 consumer_key = '9Kplg3cMsnqL3Uzo_aZrhUqwOu8a'
 consumer_secret = 'Hf9qKiMD3UWKOoMV2_uku0fdHv8a'
 
@@ -8,23 +10,37 @@ token = eumdac.AccessToken(credentials)
 datastore = eumdac.DataStore(token)
 selected_collection = datastore.get_collection('EO:EUM:DAT:MSG:HRSEVIRI')
 
-import datetime
 import shutil
+import pandas as pd
+import os
 
-start = datetime.datetime(2023, 5, 30, 10, 45)
-dt = datetime.timedelta(minutes=15)
+df = pd.read_excel('../../other_data/list_of_cases.xlsx')
+dates = pd.to_datetime(
+    df.dates[df.selected == 'x'].astype('str') + ' ' + df.h[df.selected == 'x'].astype('int').astype('str')).values
+ddir = r"C:\Users\sw825517\OneDrive - University of Reading\research\code\eumetsat"
 
-# Retrieve datasets that match our filter
-products = selected_collection.search(
-    dtstart=start,
-    dtend=start+dt)
+for start in dates:
+    # start = datetime.datetime(2023, 5, 30, 10, 45)
+    dt = np.timedelta64(15, 'm')
 
-for product in products:
-    print(str(product))
+    # Retrieve datasets that match our filter
+    products = selected_collection.search(
+        dtstart=start,
+        dtend=start + dt)
 
-for product in products:
-    with product.open() as fsrc, \
-            open(fsrc.name, mode='wb') as fdst:
-        shutil.copyfileobj(fsrc, fdst)
-        print(f'Download of product {product} finished.')
+    for product in products:
+        print(str(product))
+
+        if not os.path.exists('data/' + str(product)):
+            os.makedirs('data/' + str(product))
+
+        with product.open() as fsrc, \
+                open(fsrc.name, mode='wb') as fdst:
+            shutil.copyfileobj(fsrc, fdst)
+            print(f'Download of product {product} finished.')
+            print(f'Unpacking {fdst.name}')
+            shutil.unpack_archive(fdst.name, extract_dir=ddir + '/data/' + fdst.name[:-4])
+            print(f'Removing {fdst.name}')
+            os.remove(fdst.name)
+
 print('All downloads are finished.')
