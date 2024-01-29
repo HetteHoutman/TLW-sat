74ff6eeee2563c416d663314f46eaebf3cd6c299
HetteHoutman <h.houtman@hotmail.com>
2024-01-29 15:03:05 +0000

    works

--- wavelet_analysis.py
+++ wavelet_analysis.py
@@ -1,14 +1,12 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import py_cwt2d
 from fourier import *
 from miscellaneous import *
 from psd import periodic_smooth_decomp
-from wavelet_plot import *
+from skimage.filters import gaussian, threshold_local
 from wavelet import *
-from skimage import exposure
-from skimage.filters import gaussian
+from wavelet_plot import *
 
 from file_to_image import produce_scene
 
@@ -29,28 +27,29 @@ def get_seviri_img(settings, sat_bl, sat_tr, stripe_test=False):
     return orig, Lx, Ly
 
 
-def run(args):
+if __name__ == '__main__':
     # options
     test = False
     stripe_test = False
 
-    lambda_min = 4
-    lambda_max = 30
+    lambda_min = 5
+    lambda_max = 35
     lambda_bin_width = 1
     theta_bin_width = 5
     omega_0x = 6
-    pspec_threshold = 3e-4
-    # pspec_threshold = 2e-3
+    # pspec_threshold = 3e-4
+    pspec_threshold = 1e-2
+    block_size = 51
 
     # settings
-    check_argv_num(args, 2, "(settings, region json files)")
-    s = load_settings(args[1])
+    check_argv_num(sys.argv, 2, "(settings, region json files)")
+    s = load_settings(sys.argv[1])
     datetime = get_datetime_from_settings(s)
-    region = args[2]
+    region = sys.argv[2]
     sat_bounds = get_region_var("sat_bounds", region,
                                 r"C:/Users/sw825517/OneDrive - University of Reading/research/code/tephi_plot/regions/")
     sat_bl, sat_tr = sat_bounds[:2], sat_bounds[2:]
-    save_path = f'./plots/{datetime}/{region}/'
+    save_path = f'./plots/{datetime}/{region}/51_'
     if test:
         save_path = f'./plots/test/'
 
@@ -59,11 +58,12 @@ def run(args):
 
     # normalise orig image
     orig /= orig.max()
-    # orig = exposure.equalize_adapthist(orig, clip_limit=0.03)
+    # orig = exposure.equalize_hist(orig)
+    orig = orig > threshold_local(orig, block_size)
 
-    # define evaluation space
-    lambdas = np.arange(lambda_min, lambda_max + 1)
-    lambdas_edges = create_bins_from_midpoints(lambdas)
+    # lambdas, lambdas_edges = create_range_and_bin_edges_from_minmax([lambda_min, lambda_max],
+    #                                                                 lambda_max - lambda_min + 1)
+    lambdas, lambdas_edges = log_spaced_lambda([lambda_min, lambda_max], 1.075)
     thetas = np.arange(0, 180, theta_bin_width)
     thetas_edges = create_bins_from_midpoints(thetas)
     scales = omega_0x * lambdas / (2 * np.pi)
@@ -71,7 +71,7 @@ def run(args):
     # initialise wavelet power spectrum array and fill
     pspec = np.zeros((*orig.shape, len(lambdas), len(thetas)))
     for i, theta in enumerate(thetas):
-        cwt, wavnorm = py_cwt2d.cwt_2d(orig, scales, 'morlet', omega_0x=omega_0x, phi=np.deg2rad(90 + theta))
+        cwt, wavnorm = py_cwt2d.cwt_2d(orig, scales, 'morlet', omega_0x=omega_0x, phi=np.deg2rad(90 + theta), epsilon=1)
         pspec[..., i] = (abs(cwt) / scales / wavnorm) ** 2
 
     # calculate derived things
@@ -82,7 +82,7 @@ def run(args):
     max_pspec = np.ma.masked_less(pspec.data.max((-2, -1)), pspec_threshold)
     max_lambdas, max_thetas = max_lambda_theta(pspec.data, lambdas, thetas)
 
-    avg_pspec = pspec.data.mean((0, 1))
+    avg_pspec = np.ma.masked_less(pspec.data, pspec_threshold / 2).mean((0, 1))
 
     # histograms
     strong_hist, _, _ = np.histogram2d(strong_lambdas, strong_thetas, bins=[lambdas_edges, thetas_edges])
@@ -96,6 +96,10 @@ def run(args):
     # determine maximum in smoothed image
     idxs = np.unravel_index(strong_hist_smoothed.argmax(), strong_hist_smoothed.shape)
     result_lambda, result_theta = lambdas[idxs[0]], thetas[idxs[1]]
+
+    # avg max
+    avg_idxs = np.unravel_index(avg_pspec.argmax(), avg_pspec.shape)
+    henk_lambda, henk_theta = lambdas[avg_idxs[0]], thetas[avg_idxs[1]]
 
     # plot images
     # TODO for some reason a negative value/level is sometimes passed to colorbar here
@@ -133,10 +137,14 @@ def run(args):
     plt.savefig(save_path + 'wavelet_k_histogram_max_pspec_polar.png', dpi=300)
     plt.show()
 
+    plot_polar_pcolormesh(avg_pspec, lambdas_edges, thetas_edges, cbarlabel='Average power spectrum')
+    plt.savefig(save_path + 'wavelet_average_pspec.png', dpi=300)
+    plt.show()
+
     # save results
     if not test:
         csv_root = '../../other_data/wavelet_results/'
-        csv_file = 'sat_hist.csv'
+        csv_file = f'sat_adapt_thresh_{block_size}.csv'
 
         try:
             df = pd.read_csv(csv_root + csv_file, index_col=[0, 1, 2], parse_dates=[0], dayfirst=True)
@@ -152,6 +160,18 @@ def run(args):
         df.sort_index(inplace=True)
         df.to_csv(csv_root + csv_file)
 
+        csv_file = f'sat_test.csv'
 
-if __name__ == '__main__':
-    run(sys.argv)
+        try:
+            df = pd.read_csv(csv_root + csv_file, index_col=[0, 1, 2], parse_dates=[0], dayfirst=True)
+        except FileNotFoundError:
+            df = pd.read_csv(csv_root + 'template.csv', index_col=[0, 1, 2], parse_dates=[0], dayfirst=True)
+
+        df.sort_index(inplace=True)
+        date = pd.to_datetime(f'{s.year}-{s.month:02d}-{s.day:02d}')
+
+        df.loc[(date, region, s.h), 'lambda'] = henk_lambda
+        df.loc[(date, region, s.h), 'theta'] = henk_theta
+
+        df.sort_index(inplace=True)
+        df.to_csv(csv_root + csv_file)
--- wavelet_run_all.py
+++ wavelet_run_all.py
@@ -1,12 +1,12 @@
 import os
 import sys
-from wavelet_analysis import run
 
 import pandas as pd
 
 df = pd.read_excel(sys.argv[1], index_col=[0,1])
 
 for idx, row in df.iterrows():
-    # os.system(f"python wavelet_analysis.py ../tephi_plot/settings/{idx[0][:4]}{idx[0][5:7]}{idx[0][8:10]}_{row.hour}.json {idx[1]}")
-    args = [0, f"../tephi_plot/settings/{idx[0][:4]}{idx[0][5:7]}{idx[0][8:10]}_{row.hour}.json", f"{idx[1]}"]
-    run(args)
+    os.system(
+        f"python wavelet_analysis.py ../tephi_plot/settings/{idx[0][:4]}{idx[0][5:7]}{idx[0][8:10]}_{row.hour}.json {idx[1]}")
+    # args = [0, f"../tephi_plot/settings/{idx[0][:4]}{idx[0][5:7]}{idx[0][8:10]}_{row.hour}.json", f"{idx[1]}"]
+    # run(args)
