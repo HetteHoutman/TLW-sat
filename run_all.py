7165deffb739446fe58c226c6b052b7031356bfa
HetteHoutman <h.houtman@hotmail.com>
2024-01-30 17:15:12 +0000

    .

--- run_all.py
+++ run_all.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 import pandas as pd
 
@@ -7,3 +8,4 @@ df = pd.read_excel(sys.argv[1], index_col=[0,1])
 
 for idx, row in df.iterrows():
     os.system(f"python fourier_analysis.py ../tephi_plot/settings/{idx[0][:4]}{idx[0][5:7]}{idx[0][8:10]}_{row.hour}.json {idx[1]}")
+    # subprocess.call(["python", f"fourier_analysis.py",  f"../tephi_plot/settings/{idx[0][:4]}{idx[0][5:7]}{idx[0][8:10]}_{row.hour}.json",  f"{idx[1]}"], shell=True)
