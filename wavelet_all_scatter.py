fb439d986e598ca00ef5816912add7a3c3691ca3
HetteHoutman <h.houtman@hotmail.com>
2024-02-13 16:02:59 +0000

    better plots

--- wavelet_all_scatter.py
+++ wavelet_all_scatter.py
@@ -1,10 +1,13 @@
 import sys
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from matplotlib.ticker import StrMethodFormatter
-from miscellaneous import check_argv_num
+from miscellaneous import check_argv_num, k_spaced_lambda, create_bins_from_midpoints
+from wavelet_plot import plot_result_lambda_hist
 
+# prepare data
 check_argv_num(sys.argv, 4, '(results file 1, results file 1 name, results file 2, results file 2 name)')
 file_1 = sys.argv[1]
 filename_1 = file_1.split('/')[-1]
@@ -20,28 +23,26 @@ df2 = pd.read_csv(file_2, index_col=[0, 1, 2], parse_dates=[0]).sort_index()
 df1.sort_index()
 df2.sort_index()
 
-# lambda
-xy_line = [min(df1[['lambda', 'lambda_min', 'lambda_max']].min(axis=None),
-               df2[['lambda', 'lambda_min', 'lambda_max']].min(axis=None)),
-           max(df1[['lambda', 'lambda_min', 'lambda_max']].max(axis=None),
-               df2[['lambda', 'lambda_min', 'lambda_max']].max(axis=None))]
+df = df1.merge(df2, how='inner', left_index=True, right_index=True, suffixes=('_' + name_1, '_' + name_2))
+del (df1, df2)
+
+l = df[[f'lambda_{name_1}', f'lambda_min_{name_1}', f'lambda_max_{name_1}',
+        f'lambda_{name_2}', f'lambda_min_{name_2}', f'lambda_max_{name_2}']]
+
+t = df[[f'theta_{name_1}', f'theta_min_{name_1}', f'theta_max_{name_1}',
+        f'theta_{name_2}', f'theta_min_{name_2}', f'theta_max_{name_2}']]
+
+# plot lambda
+xy_line = [l.min(axis=None), l.max(axis=None)]
 plt.plot(xy_line, xy_line, 'k--', zorder=0)
 
-for i, idx in enumerate(df1.index):
-    try:
-        xerr = [[df1.loc[idx, 'lambda'] - df1.loc[idx, 'lambda_min']],
-                [df1.loc[idx, 'lambda_max'] - df1.loc[idx, 'lambda']]]
-        yerr = [[df2.loc[idx, 'lambda'] - df2.loc[idx, 'lambda_min']],
-                [df2.loc[idx, 'lambda_max'] - df2.loc[idx, 'lambda']]]
-        plt.errorbar(df1.loc[idx, 'lambda'], df2.loc[idx, 'lambda'],
-                     xerr=xerr,
-                     yerr=yerr,
-                     label=f'{idx[0].date()}_{idx[2]}h {idx[1]}',
-                     capsize=3, marker='s', c='k')
-    except KeyError:
-        pass
+plt.errorbar(l[f'lambda_{name_1}'], l[f'lambda_{name_2}'],
+             xerr=[l[f'lambda_{name_1}'] - l[f'lambda_min_{name_1}'],
+                   l[f'lambda_max_{name_1}'] - l[f'lambda_{name_1}']],
+             yerr=[l[f'lambda_{name_2}'] - l[f'lambda_min_{name_2}'],
+                   l[f'lambda_max_{name_2}'] - l[f'lambda_{name_2}']],
+             capsize=3, marker='s', c='k', linestyle='')
 
-# plt.legend()
 plt.title(f'{filename_1[:-4]} vs. {filename_2[:-4]}')
 plt.xlabel(f'{name_1} wavelength (km)')
 plt.ylabel(f'{name_2} wavelength (km)')
@@ -56,43 +57,46 @@ plt.tight_layout()
 plt.savefig(f'plots/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_lambda_comparison.png', dpi=300)
 plt.show()
 
-# theta
+lambdas, lambdas_edges = k_spaced_lambda([5, 35], 40)
+plot_result_lambda_hist(l[f'lambda_{name_1}'], l[f'lambda_{name_2}'], lambdas_edges,
+                        label1=f'{name_1} wavelength (km)', label2=f'{name_2} wavelength (km)')
+plt.plot(xy_line, xy_line, 'k--')
+plt.tight_layout()
+plt.savefig(f'plots/results/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_lambda_comparison_hist.png', dpi=300)
+plt.show()
+
+# plot theta
 xy_line = [0, 180]
 plt.plot(xy_line, xy_line, 'k--', zorder=0)
 
-overshoot1 = dict()
-overshoot2 = dict()
+xerr = [(t[f'theta_{name_1}'] - t[f'theta_min_{name_1}']) % 180,
+        (t[f'theta_max_{name_1}'] - t[f'theta_{name_1}']) % 180]
+yerr = [(t[f'theta_{name_2}'] - t[f'theta_min_{name_2}']) % 180,
+        (t[f'theta_max_{name_2}'] - t[f'theta_{name_2}']) % 180]
 
-for i, idx in enumerate(df1.index):
-    try:
-        xerr = [[(df1.loc[idx, 'theta'] - df1.loc[idx, 'theta_min']) % 180],
-                [(df1.loc[idx, 'theta_max'] - df1.loc[idx, 'theta']) % 180]]
-        yerr = [[(df2.loc[idx, 'theta'] - df2.loc[idx, 'theta_min']) % 180],
-                [(df2.loc[idx, 'theta_max'] - df2.loc[idx, 'theta']) % 180]]
+plt.errorbar(t[f'theta_{name_1}'], t[f'theta_{name_2}'],
+             xerr=xerr, yerr=yerr, capsize=3, marker='s', c='k', linestyle='')
 
-        plt.errorbar(df1.loc[idx, 'theta'], df2.loc[idx, 'theta'],
-                     xerr=xerr,
-                     yerr=yerr,
-                     label=f'{idx[0].date()}_{idx[2]}h {idx[1]}',
-                     capsize=3, marker='s', c='k')
+# overshoots
+mask = t[f'theta_max_{name_1}'] < t[f'theta_{name_1}']
+plt.errorbar(t[f'theta_{name_1}'][mask] - 180, t[f'theta_{name_2}'][mask],
+             xerr=[xerr[0][mask], xerr[1][mask]], yerr=[yerr[0][mask], yerr[1][mask]],
+             capsize=3, marker='s', c='k', linestyle='')
 
-        if df1.loc[idx, 'theta_max'] < df1.loc[idx, 'theta']:
-            plt.errorbar(df1.loc[idx, 'theta'] - 180, df2.loc[idx, 'theta'],
-                         xerr=xerr, yerr=yerr, capsize=3, marker='s', c='k')
+mask = t[f'theta_min_{name_1}'] > t[f'theta_{name_1}']
+plt.errorbar(t[f'theta_{name_1}'][mask] + 180, t[f'theta_{name_2}'][mask],
+             xerr=[xerr[0][mask], xerr[1][mask]], yerr=[yerr[0][mask], yerr[1][mask]],
+             capsize=3, marker='s', c='k', linestyle='')
 
-        if df1.loc[idx, 'theta_min'] > df1.loc[idx, 'theta']:
-            plt.errorbar(df1.loc[idx, 'theta'] + 180, df2.loc[idx, 'theta'],
-                         xerr=xerr, yerr=yerr, capsize=3, marker='s', c='k')
+mask = t[f'theta_max_{name_2}'] < t[f'theta_{name_2}']
+plt.errorbar(t[f'theta_{name_1}'][mask], t[f'theta_{name_2}'][mask] - 180,
+             xerr=[xerr[0][mask], xerr[1][mask]], yerr=[yerr[0][mask], yerr[1][mask]],
+             capsize=3, marker='s', c='k', linestyle='')
 
-        if df2.loc[idx, 'theta_max'] < df2.loc[idx, 'theta']:
-            plt.errorbar(df1.loc[idx, 'theta'], df2.loc[idx, 'theta'] - 180,
-                         xerr=xerr, yerr=yerr, capsize=3, marker='s', c='k')
-
-        if df2.loc[idx, 'theta_min'] > df2.loc[idx, 'theta']:
-            plt.errorbar(df1.loc[idx, 'theta'], df2.loc[idx, 'theta'] + 180,
-                         xerr=xerr, yerr=yerr, capsize=3, marker='s', c='k')
-    except KeyError:
-        pass
+mask = t[f'theta_min_{name_2}'] > t[f'theta_{name_2}']
+plt.errorbar(t[f'theta_{name_1}'][mask], t[f'theta_{name_2}'][mask] + 180,
+             xerr=[xerr[0][mask], xerr[1][mask]], yerr=[yerr[0][mask], yerr[1][mask]],
+             capsize=3, marker='s', c='k', linestyle='')
 
 # plt.legend()
 plt.title(f'{filename_1[:-4]} vs. {filename_2[:-4]}')
@@ -101,11 +105,17 @@ plt.ylabel(f'{name_2} wavevector direction from north (deg)')
 plt.xlim(0, 180)
 plt.ylim(0, 180)
 plt.tight_layout()
-plt.savefig(f'plots/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_theta_comparison.png', dpi=300)
+plt.savefig(f'plots/results/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_theta_comparison.png', dpi=300)
 plt.show()
 
-# from wavelet_plot import plot_result_lambda_hist
-# from miscellaneous import k_spaced_lambda
-#
-# plot_result_lambda_hist(df1['lambda'], df2['lambda'], k_spaced_lambda([5, 35], 40)[0])
-# plt.show()
+thetas = np.arange(0, 180, 5)
+thetas_edges = create_bins_from_midpoints(thetas)
+
+plt.hist2d(t[f'theta_{name_1}'], t[f'theta_{name_2}'], bins=[thetas_edges, thetas_edges])
+plt.plot(xy_line, xy_line, 'k--')
+plt.colorbar(label='Count')
+plt.xlabel(f'{name_1} wavevector direction from north (deg)')
+plt.ylabel(f'{name_2} wavevector direction from north (deg)')
+plt.tight_layout()
+plt.savefig(f'plots/results/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_theta_comparison_hist.png', dpi=300)
+plt.show()
