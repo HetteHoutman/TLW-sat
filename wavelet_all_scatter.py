277fb0c170554d2f810d75cf0725a45fec8aa0c1
HetteHoutman <h.houtman@hotmail.com>
2024-03-18 15:15:25 +0000

    additions

--- wavelet_all_scatter.py
+++ wavelet_all_scatter.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.ticker import StrMethodFormatter
-from miscellaneous import check_argv_num, k_spaced_lambda, create_bins_from_midpoints
+from miscellaneous import check_argv_num, k_spaced_lambda, create_bins_from_midpoints, log_spaced_lambda
 from wavelet_plot import plot_result_lambda_hist, plot_polar_pcolormesh
 from wavelet import angle_error
 
@@ -30,6 +30,13 @@ df2.sort_index()
 df = df1.merge(df2, how='inner', left_index=True, right_index=True, suffixes=('_' + name_1, '_' + name_2))
 del (df1, df2)
 
+# df = df[(df['theta_ukv'] > 120) | (df['theta_ukv'] < 60)]
+# df = df[df[f'lambda_{name_2}'] < 10]
+
+
+df['lambda_diff'] = df[f'lambda_{name_1}'] - df[f'lambda_{name_2}']
+df['theta_diff'] = angle_error(df[f'theta_{name_1}'], df[f'theta_{name_2}'])
+
 l = df[[f'lambda_{name_1}', f'lambda_min_{name_1}', f'lambda_max_{name_1}',
         f'lambda_{name_2}', f'lambda_min_{name_2}', f'lambda_max_{name_2}']]
 
@@ -40,12 +47,17 @@ t = df[[f'theta_{name_1}', f'theta_min_{name_1}', f'theta_max_{name_1}',
 xy_line = [l.min(axis=None), l.max(axis=None)]
 plt.plot(xy_line, xy_line, 'k--', zorder=0)
 
-plt.errorbar(l[f'lambda_{name_1}'], l[f'lambda_{name_2}'],
-             xerr=[l[f'lambda_{name_1}'] - l[f'lambda_min_{name_1}'],
-                   l[f'lambda_max_{name_1}'] - l[f'lambda_{name_1}']],
-             yerr=[l[f'lambda_{name_2}'] - l[f'lambda_min_{name_2}'],
-                   l[f'lambda_max_{name_2}'] - l[f'lambda_{name_2}']],
-             capsize=3, marker='s', c='k', linestyle='')
+for i, index in enumerate(df.index):
+    c = 'k' if abs(df.theta_diff.loc[index]) <= 45 else 'r'
+    plt.errorbar(l[f'lambda_{name_1}'].loc[index], l[f'lambda_{name_2}'].loc[index],
+                 xerr=[[l[f'lambda_{name_1}'].loc[index] - l[f'lambda_min_{name_1}'].loc[index]],
+                       [l[f'lambda_max_{name_1}'].loc[index] - l[f'lambda_{name_1}'].loc[index]]],
+                 yerr=[[l[f'lambda_{name_2}'].loc[index] - l[f'lambda_min_{name_2}'].loc[index]],
+                       [l[f'lambda_max_{name_2}'].loc[index] - l[f'lambda_{name_2}'].loc[index]]],
+                 capsize=3, marker='s', color=c, linestyle='', alpha=0.5)
+
+# plt.scatter(l[f'lambda_{name_1}'], l[f'lambda_{name_2}'], c=df.theta_diff, cmap='plasma', marker='s', alpha=0.5, vmin=0, vmax=90)
+# plt.colorbar()
 
 plt.title(f'{filename_1[:-4]} vs. {filename_2[:-4]}')
 plt.xlabel(f'{name_1} wavelength (km)')
@@ -61,7 +73,7 @@ plt.tight_layout()
 plt.savefig(f'plots/results/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_lambda_comparison.png', dpi=300)
 plt.close()
 
-lambdas, lambdas_edges = k_spaced_lambda(lambda_range, n_lambda)
+lambdas, lambdas_edges = log_spaced_lambda(lambda_range, (lambda_range[-1]/lambda_range[0]) ** (1 / (41 - 1)))
 plot_result_lambda_hist(l[f'lambda_{name_1}'], l[f'lambda_{name_2}'], lambdas_edges,
                         label1=f'{name_1} wavelength (km)', label2=f'{name_2} wavelength (km)')
 plt.plot(xy_line, xy_line, 'k--')
@@ -73,13 +85,15 @@ plt.close()
 xy_line = [0, 180]
 plt.plot(xy_line, xy_line, 'k--', zorder=0)
 
-xerr = [(t[f'theta_{name_1}'] - t[f'theta_min_{name_1}']) % 180,
-        (t[f'theta_max_{name_1}'] - t[f'theta_{name_1}']) % 180]
-yerr = [(t[f'theta_{name_2}'] - t[f'theta_min_{name_2}']) % 180,
-        (t[f'theta_max_{name_2}'] - t[f'theta_{name_2}']) % 180]
+xerr = np.array([(t[f'theta_{name_1}'] - t[f'theta_min_{name_1}']).values % 180,
+        (t[f'theta_max_{name_1}'] - t[f'theta_{name_1}']).values % 180])
+yerr = np.array([(t[f'theta_{name_2}'] - t[f'theta_min_{name_2}']).values % 180,
+        (t[f'theta_max_{name_2}'] - t[f'theta_{name_2}']).values % 180])
 
-plt.errorbar(t[f'theta_{name_1}'], t[f'theta_{name_2}'],
-             xerr=xerr, yerr=yerr, capsize=3, marker='s', c='k', linestyle='')
+for i, index in enumerate(df.index):
+    plt.errorbar(t[f'theta_{name_1}'].loc[index], t[f'theta_{name_2}'].loc[index],
+                 xerr=xerr[:, i].reshape(2,1), yerr=yerr[:, i].reshape(2,1), capsize=3, marker='s',
+                 color=plt.cm.RdBu((df.lambda_diff.loc[index] - df.lambda_diff.mean())/np.abs(df.lambda_diff.max()) ), linestyle='', alpha=0.3)
 
 # overshoots
 mask = t[f'theta_max_{name_1}'] < t[f'theta_{name_1}']
@@ -103,6 +117,7 @@ plt.errorbar(t[f'theta_{name_1}'][mask], t[f'theta_{name_2}'][mask] + 180,
              capsize=3, marker='s', c='k', linestyle='')
 
 # plt.legend()
+# plt.colorbar()
 plt.title(f'{filename_1[:-4]} vs. {filename_2[:-4]}')
 plt.xlabel(f'{name_1} wavevector direction from north (deg)')
 plt.ylabel(f'{name_2} wavevector direction from north (deg)')
@@ -115,7 +130,7 @@ plt.close()
 thetas = np.arange(0, 180, 5)
 thetas_edges = create_bins_from_midpoints(thetas)
 
-plt.hist2d(t[f'theta_{name_1}'], t[f'theta_{name_2}'], bins=[thetas_edges, thetas_edges])
+plt.hist2d(t[f'theta_{name_1}'], t[f'theta_{name_2}'], bins=[thetas_edges, thetas_edges], cmin=1)
 plt.plot(xy_line, xy_line, 'k--')
 plt.colorbar(label='Count')
 plt.xlabel(f'{name_1} wavevector direction from north (deg)')
@@ -124,11 +139,10 @@ plt.tight_layout()
 plt.savefig(f'plots/results/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_theta_comparison_hist.png', dpi=300)
 plt.close()
 
+
+
 #df.reset_index()['date'].dt.month.value_counts().sort_index().plot(kind='bar')
 # df.reset_index()['region'].value_counts().plot(kind='bar')
-
-df['lambda_diff'] = df[f'lambda_{name_1}'] - df[f'lambda_{name_2}']
-df['theta_diff'] = angle_error(df[f'theta_{name_1}'], df[f'theta_{name_2}'])
 
 plt.scatter(df['lambda_diff']/df[f'lambda_{name_1}'], df['theta_diff'])
 plt.xlabel(f'(lambda_{name_1} - lambda_{name_2}) / lambda_{name_1}')
@@ -154,8 +168,10 @@ plt.ylabel(f'theta_{name_1} - theta_{name_2} (deg)')
 plt.tight_layout()
 plt.close()
 
-plt.hist(df.lambda_sat, bins=np.linspace(*lambda_range, 21), label=name_1)
-plt.hist(df.lambda_ukv, hatch='/', fill=False,  bins=np.linspace(*lambda_range, 21), label=name_2)
+lambdas_edges = np.linspace(*lambda_range, 21)
+
+plt.hist(df[f'lambda_{name_1}'], bins=lambdas_edges, label=name_1)
+plt.hist(df[f'lambda_{name_2}'], hatch='/', fill=False, bins=lambdas_edges, label=name_2)
 plt.xlabel('Wavelength (km)')
 plt.ylabel('Count')
 plt.legend()
@@ -163,8 +179,8 @@ plt.tight_layout()
 plt.savefig(f'plots/results/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_lambda_hist.png')
 plt.close()
 
-plt.hist(df.theta_sat, bins=thetas_edges, label=name_1)
-plt.hist(df.theta_ukv, hatch='/', fill=False,  bins=thetas_edges, label=name_2)
+plt.hist(df[f'theta_{name_1}'], bins=thetas_edges, label=name_1)
+plt.hist(df[f'theta_{name_2}'], hatch='/', fill=False,  bins=thetas_edges, label=name_2)
 plt.xlabel('Orientation (deg)')
 plt.ylabel('Count')
 plt.legend()
@@ -172,13 +188,16 @@ plt.tight_layout()
 plt.savefig(f'plots/results/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_theta_hist.png')
 plt.close()
 
+lambdas, lambdas_edges = log_spaced_lambda(lambda_range, (lambda_range[-1]/lambda_range[0]) ** (1 / (21 - 1)))
 hist_1 = np.histogram2d(df[f'lambda_{name_1}'], df[f'theta_{name_1}'], bins=[lambdas_edges, thetas_edges])
+hist_1[0][hist_1[0]==0] = np.nan
 hist_2 = np.histogram2d(df[f'lambda_{name_2}'], df[f'theta_{name_2}'], bins=[lambdas_edges, thetas_edges])
+hist_2[0][hist_2[0] == 0] = np.nan
 
-plot_polar_pcolormesh(*hist_1, cbarlabel='Count', vmax=np.max([hist_1[0], hist_2[0]]))
+plot_polar_pcolormesh(*hist_1, cbarlabel='Count', vmax=np.nanmax([hist_1[0], hist_2[0]]))
 plt.savefig(f'plots/results/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_{name_1}_2dhist.png')
 plt.show()
 
-plot_polar_pcolormesh(*hist_2, cbarlabel='Count', vmax=np.max([hist_1[0], hist_2[0]]))
+plot_polar_pcolormesh(*hist_2, cbarlabel='Count', vmax=np.nanmax([hist_1[0], hist_2[0]]))
 plt.savefig(f'plots/results/wavelet_{filename_1[:-4]}_vs_{filename_2[:-4]}_{name_2}_2dhist.png')
 plt.show()
